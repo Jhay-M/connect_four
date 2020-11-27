5eba53c8a6311651d302139f463e07edc2564a96
Joshua Malomo <rexunborn@gmail.com>
2020-11-27 14:58:26 +0100

    Done with C-4 logic

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,43 +1,129 @@
 class Game
-  attr_accessor :grid
-  attr_reader :p1, :p2
+  @grid = Array.new(42).each_slice(6).to_a
 
-  def initialize
-    @p1 = create_player('Josh', 'X')
-    @p2 = create_player('Sam', 'O')
-    @grid = Array.new(7, Array.new(6))
-  end
-
-  def create_player(name, token)
-    Player.new(name, token)
+  class << self
+    attr_accessor :grid
   end
 
   def invalid?(int)
     int.negative? || int >= 7
   end
-
-  def launch
-    p1.play()
-  end
 end
 
 class Player < Game
-  attr_accessor :turns, :moves
   attr_reader :name, :token
 
   def initialize(name, token)
     @name = name
     @token = token
-    @turns = 0
-    @moves = []
   end
 
   def play(int)
-    return 'Invalid Input' if invalid?(int)
+    run(int)
+    check_win ? game_over : show_map
+  end
 
-    grid[int - 1] << token
+  def map
+    Game.grid
+  end
+
+  def show_map
+    map.each { |arr| p arr }
+  end
+
+  def run(int)
+    int -= 1
+    return 'Invalid Input' if invalid?(int)
+    return 'Column Full' unless map[int].include?(nil)
+
+    i = 0
+    while i < map[int].size
+      if map[int][i].nil?
+        map[int][i] = token
+        break
+      end
+      i += 1
+    end
+    map
+  end
+
+  def hor_wincon(i, j)
+    (map[i][j] == token) && (map[i + 1][j] == token) && (map[i + 2][j] == token) && (map[i + 3][j] == token)
+  end
+
+  def ver_wincon(i, j)
+    (map[i][j] == token) && (map[i][j + 1] == token) && (map[i][j + 3] == token) && (map[i][j + 3] == token)
+  end
+
+  def diagonal_left_wincon(i, j)
+    (map[i][j] == token) && (map[i - 1][j + 1] == token) && (map[i - 2][j + 2] == token) && (map[i - 3][j + 3] == token)
+  end
+
+  def diagonal_right_wincon(i, j)
+    (map[i][j] == token) && (map[i + 1][j + 1] == token) && (map[i + 2][j + 2] == token) && (map[i + 3][j + 3] == token)
+  end
+
+  def hor_check
+    i = 0
+    while i < map.size - 3
+      j = 0
+      while j < map[i].size
+        return true if hor_wincon(i, j)
+
+        j += 1
+      end
+      i += 1
+    end
+  end
+
+  def ver_check
+    i = 0
+    while i < map.size
+      j = 0
+      while j < map[i].size - 3
+        return true if ver_wincon(i, j)
+
+        j += 1
+      end
+      i += 1
+    end
+  end
+
+  def diagonal_left_check
+    i = 3
+    while i < map.size
+      j = 0
+      while j < map[i].size - 3
+        return true if diagonal_left_wincon(i, j)
+
+        j += 1
+      end
+      i += 1
+    end
+  end
+
+  def diagonal_right_check
+    i = 0
+    while i < map.size - 3
+      j = 0
+      while j < map[i].size - 3
+        return true if diagonal_right_wincon(i, j)
+
+        j += 1
+      end
+      i += 1
+    end
+  end
+
+  def check_win
+    # Horizontal, Vertical and Diagonal checks in both directions
+    return true if hor_check || ver_check || diagonal_left_check || diagonal_right_check
+
+    false
+  end
+
+  def game_over
+    puts "#{name} wins!"
+    self.map = Array.new(42).each_slice(6).to_a
   end
 end
-
-new_game = Game.new
-p new_game
